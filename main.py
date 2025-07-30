1054eb6c0c9c2299ef892f38fe26da230bd0b717
Invisibleghost34 <maksim.ohorzin512@gmail.com>
2025-07-30 23:30:35 +0200

    Basic movement

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 import pygame 
+from core.player import Player
 
-print("Hello world")
+#constants 
+FPS = 60 
+
+def main(): 
+
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    running = True 
+    clock = pygame.time.Clock()
+    
+
+    player = Player(0, 0, 30, 30)
+
+    while running:
+        dt = clock.tick(FPS) / 1000 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+        screen.fill("blue")
+
+        
+
+        player.input_manager(dt)
+        player.render(screen)
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+        main()
