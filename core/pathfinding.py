7f1d39539ab80f455c4f8d33167630bfd0e62ae1
Invisibleghost34 <maksim.ohorzin512@gmail.com>
2025-08-01 14:11:12 +0200

    added pathfinding todo fix it

--- core/pathfinding.py
+++ core/pathfinding.py
@@ -1,4 +1,5 @@
 import pygame, sys 
+import numpy
 from pathfinding.core.grid import Grid 
 from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
